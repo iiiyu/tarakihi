2c65e13dafcc78643dd9bff607ccf2e6e90f8f5a
Ewan Xiao <apple.iiiyu@gmail.com>
2023-12-10 22:12:19 +1300

    test6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
-// import Link from "next/link";
+import Link from "next/link";
 
 // import { CreatePost } from "~/app/_components/create-post";
-// import { getServerAuthSession } from "~/server/auth";
-// import { api } from "~/trpc/server";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
 
-export default function Home() {
-  // const hello = await api.post.hello.query({ text: "from tRPC" });
-  // const session = await getServerAuthSession();
+export default async function Home() {
+  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const session = await getServerAuthSession();
 
   return (
     <main>
       <h1 className="text-5xl">hello world</h1>
+      <div className="flex flex-col items-center gap-2">
+        <p className="text-2xl text-white">
+          {hello ? hello.greeting : "Loading tRPC query..."}
+        </p>
+
+        <div className="flex flex-col items-center justify-center gap-4">
+          <p className="text-center text-2xl text-white">
+            {session && <span>Logged in as {session.user?.name}</span>}
+          </p>
+          <Link
+            href={session ? "/api/auth/signout" : "/api/auth/signin"}
+            className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+          >
+            {session ? "Sign out" : "Sign in"}
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
