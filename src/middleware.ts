2223a34b7c5a44737084f7be2b687b12704e4f79
Ewan Xiao <apple.iiiyu@gmail.com>
2024-01-01 00:51:16 +1300

    add log

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ import { api } from "~/trpc/server";
 export default withAuth({
   callbacks: {
     authorized: async ({ req }) => {
+      console.log(req)
       const sessionToken = req.cookies.get("next-auth.session-token")?.value;
       if (!sessionToken) {
         return false;
